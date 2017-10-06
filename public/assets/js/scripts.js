b485bc788accea84dc87b73e40b5daa486d2af6e
Melissa <mwebb1037@gmail.com>
2017-10-06 00:38:41 -0400

    form html and styles

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -3,6 +3,22 @@
 //Initialize character counter
 $(document).ready(function() {
     $('input#input_text, textarea#textarea1').characterCounter();
+
+    $("#SciFi").click(function(){
+    	$(this).css('background-image', "")
+    });
+
+    $("#mystery").click(function(){
+    	$(this).css('background-image', "")
+    });
+
+    $("#adv").click(function(){
+    	$(this).css('background-image', "")
+    });
+
+    $("#vamps").click(function(){
+    	$(this).css('background-image', "")
+    });
   });
 
 //Dynamically inserts date
