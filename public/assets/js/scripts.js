c3a3fb4492c94b773bf370428606fd1a22bb5177
Ross Blair <rblair33@gmail.com>
2017-10-07 11:52:29 -0400

    First try with all docs in one place.

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,22 @@
+//Scripts for HTML files
+
+//Initialize character counter
+/*$(document).ready(function() {
+    $('input#input_text, textarea#textarea1').characterCounter();
+    $("#SciFi").click(function(){
+      $(this).css('background-image', "")
+    });
+    $("#mystery").click(function(){
+      $(this).css('background-image', "")
+    });
+    $("#adv").click(function(){
+      $(this).css('background-image', "")
+    });
+    $("#vamps").click(function(){
+      $(this).css('background-image', "")
+    });
+  });
+//Dynamically inserts date
+var today = new Date();
+document.getElementById("date").textContent = today.getFullYear();
+*/
